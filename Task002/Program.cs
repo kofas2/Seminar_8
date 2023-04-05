1d62396e38bd6d26799217c3f7b3706a5c00223c
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-04-05 21:58:39 +0300

    add Task002 Seminar 8

---
+++ Task002/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+void FillArray(int [,] array)
+{
+    for (int i = 0;i< array.GetLength(0);i++)
+    {
+        for (int j = 0;j<array.GetLength(1);j++ )
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i =0;i<array.GetLength(0);i++)
+    {
+        for (int j=0;j<array.GetLength(1);j++)
+        {
+            Console.Write($"{array[i,j],3} \t");
+        
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReverseArray(int [,] array, int [,] array2)
+{
+    for (int i =0;i<array.GetLength(0);i++)
+    {
+        for (int j=0;j<array.GetLength(1);j++)
+        {
+            
+            array2 [j,i] = array[i,j];
+        }
+    }
+}
+
+
+
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [rows,cols];
+int [,] array2 = new int [rows,cols];
+Console.WriteLine();
+if (rows == cols)
+{
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ReverseArray(array,array2);
+PrintArray(array2);
+}
+else
+{
+Console.WriteLine("Массив должен быть квадратным");
+}
