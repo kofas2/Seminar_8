072b2ead27079a857316781d1cf87678b093ce22
kofas2 <55452453+kofas2@users.noreply.github.com>
2023-04-06 16:16:35 +0300

    add Task004 Seminar 8

---
+++ Task004/Program.cs
@@ -0,0 +1,113 @@
+﻿// Задача 59: Задайте двумерный массив из целых чисел. 
+// Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива.
+// Например, задан массив:
+// 1 4 7 2    
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Наименьший элемент - 1, на выходе получим 
+// следующий массив:
+// 9  2  3
+// 4  2  4
+// 2  6  7
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],3} \t");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+int minRow = 0;
+int minCol = 0;
+Console.WriteLine();
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+int min = FindMinElement(array);
+
+
+
+
+
+int FindMinElement(int[,] array)
+{
+    int min = array[0, 0];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < min)
+            {
+                min = array[i, j];
+                minRow = i;
+                minCol = j;
+            }
+        }
+    }
+    return min;
+}
+
+
+bool[,] array2 = new bool[rows, cols];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (j != minCol && i != minRow)
+        {
+            array2[i, j] = true;
+
+        }
+        else
+        {
+            array2[i, j] = false;
+
+        }
+    }
+}
+PrintArray2(array, array2);
+
+
+void PrintArray2(int[,] array, bool [,] array2)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array2[i,j] != false)
+            {
+                Console.Write(array[i,j] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+    
+}
